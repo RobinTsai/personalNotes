aa58e0fb3bcd25bf5bf5deb583ac8c241fa770a5
caishiheng <caishiheng@udesk.cn>
2023-03-17 12:56:18 +0800

    use sh -c to fix err no such file or directory

--- shell/someElse/swiss_knife.sh
+++ shell/someElse/swiss_knife.sh
@@ -1,32 +1,31 @@
 alias self="mkdir -p /tmp/webuser/robincai; cd /tmp/webuser/robincai; pwd"
+alias ccps="cd /usr/local/kylin_cti/current; pwd"
 alias logCcps="cd /var/log/kylin_cti; pwd"
 alias logFs="cd /usr/local/freeswitch/log; pwd"
+alias ll="ls -htrl"
 
 ossBin="echo" # as default
-res=`whereis oss2mgr-linux`
-res=${res##*:}
-if [[ $res > "   " ]]; then
-    ossBin=oss2mgr-linux
-else
-    res=`whereis oss2mgr`
-    res=${res##*:}
-    if [[ $res > "   " ]]; then
-        ossBin=oss2mgr
+for name in "oss2mgr-linux" "oss2mgr"
+do
+    res=`whereis ${name}`
+    if [[ ${res##*:} > "   " ]]; then
+        ossBin=`expr match $res ".*: \(\/[^ ]*${name}\)"`
+        break
     fi
-fi
+done
 
 echo "oss bin is ${ossBin}"
 
 function ossUpload {
     filename=${1##*\/}
-    cmdStr="${ossBin} -cmd up -obj ccps/robincai/${filename} -file ${1}"
+    cmdStr=${ossBin} -cmd up -obj ccps/robincai/${filename} -file ${1}
     echo ">>> run ${cmdStr}"
-    ${cmdStr}
+    sh -c ${cmdStr}
 }
 
 function ossDownload {
     filename=${1##*\/}
     cmdStr=${ossBin} -cmd down -obj ccps/robincai/${filename} -file ${1}
     echo ">>> run ${cmdStr}"
-    ${cmdStr}
+    sh -c ${cmdStr}
 }
