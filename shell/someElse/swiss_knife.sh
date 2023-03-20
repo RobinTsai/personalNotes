b513914c22aad829e0ac2a78417b693b89176ff6
caishiheng <caishiheng@udesk.cn>
2023-03-20 13:42:39 +0800

    fix swiss knife

--- shell/someElse/swiss_knife.sh
+++ shell/someElse/swiss_knife.sh
@@ -9,7 +9,7 @@ for name in "oss2mgr-linux" "oss2mgr"
 do
     res=`whereis ${name}`
     if [[ ${res##*:} > "   " ]]; then
-        ossBin=`expr match $res ".*: \(\/[^ ]*${name}\)"`
+        ossBin=`expr match "$res" ".*: \(\/[^ ]*${name}\)"`
         break
     fi
 done
@@ -18,14 +18,14 @@ echo "oss bin is ${ossBin}"
 
 function ossUpload {
     filename=${1##*\/}
-    cmdStr=${ossBin} -cmd up -obj ccps/robincai/${filename} -file ${1}
+    cmdStr="${ossBin} -cmd up -obj ccps/robincai/${filename} -file ${1}"
     echo ">>> run ${cmdStr}"
-    sh -c ${cmdStr}
+    sh -c "${cmdStr}"
 }
 
 function ossDownload {
     filename=${1##*\/}
-    cmdStr=${ossBin} -cmd down -obj ccps/robincai/${filename} -file ${1}
+    cmdStr="${ossBin} -cmd down -obj ccps/robincai/${filename} -file ${1}"
     echo ">>> run ${cmdStr}"
-    sh -c ${cmdStr}
+    sh -c "${cmdStr}"
 }
